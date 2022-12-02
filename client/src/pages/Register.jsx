3f8acddda860765bac63d61a50bf082a5231efb6
Dmytro Voronov <dimolg22@gmail.com>
2022-12-02 15:12:14 +0200

    add register/login page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,76 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Wrapper from "../assets/wrappers/RegisterPage";
+import Logo from "../components/Logo";
+import FormRow from "../components/FormRow";
+import Alert from "../components/Alert";
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+    showAlert: false
+}
 
 const Register = () => {
+    const [values, setValues] = useState(initialState)
+
+    const handleChange = (e) => {
+       setValues((prevState) => (
+           {...prevState,[e.target.name]: e.target.value}
+       ))
+    }
+    const onSubmit = (e) => {
+      e.preventDefault()
+        console.log(values)
+    }
+    const toggleMember = () => {
+      setValues({...values, isMember: !values.isMember})
+    }
+
+    useEffect(()=> {
+
+    }, [])
+
     return (
-        <div>
-            Register
-        </div>
+        <Wrapper className={'full-page'}>
+            <form className={'form'} onSubmit={onSubmit}>
+                <Logo/>
+                <h3>{values.isMember ? 'Login': 'Register'}</h3>
+                {values.showAlert && <Alert/>}
+                {!values.isMember && (
+                    <FormRow
+                        name={'name'}
+                        value={values.name}
+                        type={'text'}
+                        handleChange={handleChange}
+                        labelText={'name'}
+                    />
+                )}
+                <FormRow
+                    name={'email'}
+                    value={values.email}
+                    type={'email'}
+                    handleChange={handleChange}
+                    labelText={'email'}
+                />
+                <FormRow
+                    name={'password'}
+                    value={values.password}
+                    type={'password'}
+                    handleChange={handleChange}
+                    labelText={'password'}
+                />
+                <button type={'submit'} className={'btn btn-block'}>submit</button>
+                <p>
+                    {values.isMember ? 'Not a member yet?': 'Already a member?'}
+                    <button onClick={toggleMember} className={'member-btn'} type={'button'}>
+                        {values.isMember ? 'Register': 'Login'}
+                    </button>
+                </p>
+
+            </form>
+        </Wrapper>
     );
 };
 
