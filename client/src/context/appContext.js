a7ebcc7dcb5b31b94062d6596ef049e31221f6e3
Dmytro Voronov <dimolg22@gmail.com>
2022-12-04 17:36:51 +0200

    register user front-end

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,13 +1,21 @@
 import {createContext, useContext, useReducer, useState} from "react";
 import reducer from "./reducer";
-import {CLEAR_ALERT, DISPLAY_ALERT} from "./actions";
+import {CLEAR_ALERT, DISPLAY_ALERT, REGISTER_USER_BEGIN, REGISTER_USER_ERROR, REGISTER_USER_SUCCESS} from "./actions";
+import axios from "axios";
 
+const token = localStorage.getItem('token')
+const user = localStorage.getItem('user')
+const userLocation = localStorage.getItem('location')
 
 export const initialState = {
     isLoading: false,
     showAlert: false,
     alertText: '',
-    alertType: ''
+    alertType: '',
+    user: user? JSON.parse(user): null,
+    token: token,
+    userLocation: userLocation || '',
+    jobLocation: userLocation || ''
 }
 
 
@@ -25,9 +33,37 @@ const AppProvider = ({children}) => {
             dispatch({type: CLEAR_ALERT})
         }, 3000)
     }
+    const addUserToLocalStorage = ({user, token, location}) => {
+       localStorage.setItem('user', JSON.stringify(user))
+       localStorage.setItem('token',token)
+       localStorage.setItem('location', location)
+    }
+    const removeUserToLocalStorage = ({user, token, location}) => {
+        localStorage.removeItem('user')
+        localStorage.removeItem('token')
+        localStorage.removeItem('location')
+    }
+
+    const registerUser = async (currentUser) => {
+        dispatch({type: REGISTER_USER_BEGIN})
+        try {
+          const response = await axios.post('/api/v1/auth/register', currentUser)
+            // console.log(response)
+            const {user, token, location} = response.data
+            dispatch({type: REGISTER_USER_SUCCESS, payload: {user, location, token}})
+            addUserToLocalStorage({user, token, location})
+        }catch (e) {
+            dispatch({type: REGISTER_USER_ERROR, payload: e.response.data.message})
+
+
+        }
+        clearAlert()
+        // console.log(currentUser)
+    }
+
 
     return (
-        <AppContext.Provider value={{...state, displayAlert}}>
+        <AppContext.Provider value={{...state, displayAlert, registerUser}}>
             {children}
         </AppContext.Provider>
     )
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import Logo from "../components/Logo";
 import FormRow from "../components/FormRow";
 import Alert from "../components/Alert";
 import {useAppContext} from "../context/appContext";
+import {useNavigate} from "react-router-dom";
 
 const initialState = {
     name: '',
@@ -15,9 +16,17 @@ const initialState = {
 
 const Register = () => {
     const [values, setValues] = useState(initialState)
+    const navigate = useNavigate()
 
-    const {isLoading, showAlert, displayAlert} = useAppContext()
+    const {isLoading, showAlert, displayAlert, registerUser, user} = useAppContext()
 
+    useEffect(()=> {
+        if (user){
+            setTimeout(()=> {
+                navigate('/')
+            }, 3000)
+        }
+    }, [user, navigate])
 
     const handleChange = (e) => {
        setValues((prevState) => (
@@ -31,6 +40,13 @@ const Register = () => {
             displayAlert()
             return
         }
+        const currentUser = {name, email, password}
+        if (isMember){
+            console.log('already a member')
+        }else {
+            registerUser(currentUser)
+        }
+
     }
     const toggleMember = () => {
       setValues({...values, isMember: !values.isMember})
@@ -69,7 +85,7 @@ const Register = () => {
                     handleChange={handleChange}
                     labelText={'password'}
                 />
-                <button type={'submit'} className={'btn btn-block'}>submit</button>
+                <button type={'submit'} disabled={isLoading} className={'btn btn-block'}>submit</button>
                 <p>
                     {values.isMember ? 'Not a member yet?': 'Already a member?'}
                     <button onClick={toggleMember} className={'member-btn'} type={'button'}>
