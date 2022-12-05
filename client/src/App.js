1cf51a81e36d7afa6a84797631abd0f392aed8e8
Dmytro Voronov <dimolg22@gmail.com>
2022-12-05 16:41:24 +0200

    add protected route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import Landing from "./pages/Landing";
 import {Route, Routes} from "react-router-dom";
 import Register from "./pages/Register";
 import Error from "./pages/Error";
-import Dashboard from "./pages/Dashboard";
+import Stats from "./pages/dashboard/Stats";
+import AllJobs from "./pages/dashboard/AllJobs";
+import AddJob from "./pages/dashboard/AddJob";
+import Profile from "./pages/dashboard/Profile";
+import SharedLayout from "./pages/dashboard/SharedLayout";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
     <Routes >
-     <Route path={'/'} element={<Dashboard/>}/>
+     <Route path={'/'} element={<ProtectedRoute><SharedLayout/></ProtectedRoute>}>
+         <Route index element={<Stats/>}/>
+         <Route path={'all-jobs'} element={<AllJobs/>}/>
+         <Route path={'add-jobs'} element={<AddJob/>}/>
+         <Route path={'profile'} element={<Profile/>}/>
+
+     </Route>
      <Route path={'/register'} element={<Register/>}/>
      <Route path={'/landing'} element={<Landing/>}/>
      <Route path={'*'} element={<Error/>}/>
