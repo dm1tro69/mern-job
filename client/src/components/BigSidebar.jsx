9f946fdddfa88d95b4742291407348a1046a5b08
Dmytro Voronov <dimolg22@gmail.com>
2022-12-06 17:26:21 +0200

    sidebar

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Wrapper from "../assets/wrappers/BigSidebar";
+import {useAppContext} from "../context/appContext";
+import NavLinks from "./NavLinks";
+import Logo from "./Logo";
 
 const BigSidebar = () => {
+    const {showSidebar} = useAppContext()
     return (
         <Wrapper>
-            BigSidebar
+            <div className={showSidebar ? 'sidebar-container': 'sidebar-container show-sidebar'}>
+            <div className="content">
+                <header><Logo/></header>
+
+            </div>
+            <NavLinks/>
+            </div>
         </Wrapper>
     );
 };
