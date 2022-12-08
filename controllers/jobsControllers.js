09c475bb42ce19adfd4aad7d237ef3b96ec9cbdf
Dmytro Voronov <dimolg22@gmail.com>
2022-12-08 16:12:11 +0200

    job model

--- controllers/jobsControllers.js
+++ controllers/jobsControllers.js
@@ -1,5 +1,15 @@
+import {BadRequestError} from "../errors/index.js";
+import Job from "../models/Job.js";
+import {StatusCodes} from "http-status-codes";
+
 export const createJob = async (req, res) => {
-   res.send('createJob')
+    const {position, company} = req.body
+    if (!position || !company){
+        throw new BadRequestError('Please Provide All Values')
+    }
+    req.body.createdBy = req.user.userId
+    const job = Job.create(req.body)
+   res.status(StatusCodes.CREATED).json({job})
 }
 export const deleteJob = async(req, res) => {
     res.send('deleteJob')
--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import {deleteJob, getAllJobs, showStats, updateJob} from "../controllers/jobsControllers.js";
+import {createJob, deleteJob, getAllJobs, showStats, updateJob} from "../controllers/jobsControllers.js";
 import {authenticateUser} from "../middleware/auth.js";
 
 const router = Router()
 
 router.route('/').get(getAllJobs)
+router.route('/').post(authenticateUser, createJob)
 router.route('/stats').get(authenticateUser,showStats)
 router.route('/:id').delete(deleteJob).patch(updateJob)
 
